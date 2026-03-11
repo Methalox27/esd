d1f44b3d60744e82d68c17b019f5395e6fc174b0
Methalox27 <161614369+Methalox27@users.noreply.github.com>
2026-03-11 17:33:08 +0530

    Create led_toggle_user_Button.c

---
+++ led_toggle_user_Button.c
@@ -0,0 +1,12 @@
+while (1)
+  {
+    // Check if the button is pressed (State goes to RESET/LOW)
+    if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET) 
+    {
+      // Toggle the inbuilt LED
+      HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5); 
+      
+      // A small delay for "debouncing" so it doesn't toggle multiple times per press
+      HAL_Delay(250); 
+    }
+  }
