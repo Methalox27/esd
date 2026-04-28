30cfe4826296656e9686506fb5f0d169403c40cc
Methalox27 <161614369+Methalox27@users.noreply.github.com>
2026-04-28 17:25:17 +0530

    Update 9 RGB.c

--- 9 RGB.c
+++ 9 RGB.c
@@ -4,21 +4,21 @@ while (1)
 {
   //There's another line here
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, SET);
-  delay(500);
+  HAL_Delay(500);
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, RESET);
 
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, SET);
-  delay(500);
+  HAL_Delay(500);
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, RESET);
 
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_5, SET);
-  delay(500);
+  HAL_Delay(500);
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_5, RESET);
 
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, SET);
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, SET);
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_5, SET);
-  delay(1000);
+  HAL_Delay(1000);
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, RESET);
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, RESET);
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_5, RESET);
